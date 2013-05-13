9926667785b5b7853cf3d3fbaea4fadc8ed618e9
Oliver Foggin <oliverfoggin@me.com>
2013-05-13 19:25:33 +0100

    Initial commit

---
+++ CameraTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CameraTest
+//
+//  Created by Oliver Foggin on 05/13/13.
+//  Copyright (c) 2013 Oliver Foggin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
